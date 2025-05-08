6fe3192aa1c7eb74779765337cc8fee0163be115
matsuba matsuba51 <mk.21s@rakumail.jp>
2025-05-08 13:40:45 +0900

    アセットパスとwebpackの設定を更新

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -10,7 +10,7 @@
     <!-- 以下、Bootstrap 5 関連 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="{{ mix('build/assets/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="font-sans antialiased">
@@ -36,7 +36,7 @@
             </div>
         </main>
 
-        <script src="{{ mix('build/assets/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
         <footer>
             <p class="footer-copyright">&copy; {{ date('Y') }} Surfing Information. All Rights Reserved.</p>
         </footer>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,8 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/build/assets')
-   .css('resources/css/app.css', 'public/build/assets')
+mix.js('resources/js/app.js', 'public/js')
+   .css('resources/css/app.css', 'public/css')
+   .version()  // バージョン管理を有効にする
    .webpackConfig({
       resolve: {
         extensions: ['.js', '.jsx', '.json']

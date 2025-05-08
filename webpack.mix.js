4a2b536701927cacbc6a26bcdb11d9489eb3dd32
matsuba matsuba51 <mk.21s@rakumail.jp>
2025-05-08 15:24:32 +0900

    webpackの設定を修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/build/assets')
-   .css('resources/css/app.css', 'public/build/assets')
+mix.js('resources/js/app.js', 'public/build/assets/app.js')
+   .css('resources/css/app.css', 'public/build/assets/app.css')
    .version()
    .webpackConfig({
       resolve: {

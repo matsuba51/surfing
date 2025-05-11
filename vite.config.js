44da4d9a8a1cfb0e0b0aec380c36f25fe75e5505
matsuba matsuba51 <mk.21s@rakumail.jp>
2025-05-11 22:42:33 +0900

    vite.config.js を更新してビルド設定を調整

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: '/',  
+  base: '/', 
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -11,5 +11,6 @@ export default defineConfig({
   ],
   build: {
     manifest: true,
+    outDir: 'public/assets', 
   },
 });
